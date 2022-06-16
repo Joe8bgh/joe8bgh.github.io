99a75372b06902cf826f91a9bc3f664d1558f58a
Joe8bgh <jdelisi01@gmail.com>
2022-06-16 14:23:27 -0400

    Update script.js

--- script.js
+++ script.js
@@ -18,10 +18,11 @@ function handleFormSubmit(event) {
   xhr.open("PUT", "https://np9u4av9mc.execute-api.us-east-1.amazonaws.com/items" , true);
   xhr.setRequestHeader('Content-Type', '*');
   xhr.send(JSON.stringify({
-      'id': 'NewProject',
+      'id': 'abcdef234',
       'price': 12345,
       'name': 'myitem'
   }));
+
 }
 
 var x = 1;
