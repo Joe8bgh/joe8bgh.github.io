02facb3b6572411edc5ecc376ef9ba1f1e988e9b
Joe8bgh <jdelisi01@gmail.com>
2022-06-16 15:09:55 -0400

    Update script.js

--- script.js
+++ script.js
@@ -15,12 +15,11 @@ function handleFormSubmit(event) {
   console.log(project_json);
 
   const json = '{"id":abcdef234, "price":42, "name":myItem}';
-  const obj = JSON.parse(json);
 
   var xhr = new XMLHttpRequest();
   xhr.open("PUT", "https://np9u4av9mc.execute-api.us-east-1.amazonaws.com/items" , true);
   xhr.setRequestHeader('Content-Type', '*');
-  xhr.send(obj);
+  xhr.send(JSON.parse(json));
 }
 
 var x = 1;
