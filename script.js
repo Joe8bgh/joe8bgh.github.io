d0d80cc55e48dd3955d4facd99ede37dc6760772
Joe8bgh <jdelisi01@gmail.com>
2022-06-20 14:50:22 -0400

    Update v1.0

--- script.js
+++ script.js
@@ -9,222 +9,300 @@ function handleFormSubmit(event) {
   //formJSON.snacks = data.getAll('snacks');
 
   const results = document.querySelector('.results pre');
+
   results.innerText = JSON.stringify(formJSON, null, 2);
+  //console.log(formJSON);
 
   const project_json = JSON.stringify(formJSON, null, 2);
-  console.log(project_json);
+  //console.log(project_json);
 
-  var xhr = new XMLHttpRequest();
-  xhr.open("PUT", "https://b1ljlllud4.execute-api.us-east-1.amazonaws.com/items" , true);
-  xhr.send(project_json);
+  var myJSONString = JSON.stringify(project_json);
+  myJSONString = myJSONString.toString();
+  myJSONString = myJSONString.replace(/{|}/g, '');
+  myJSONString = myJSONString.replace(/"/g, '');
+  myJSONString = myJSONString.replace(/\\/g, '');
+  myJSONString = myJSONString.replace(/n  /g, '');
+
+  //console.log(myJSONString);
+
+  let content = '"project": "';
+  content += myJSONString;
+  //console.log(content);
+
+  let top = '{';
+  top += "\n";
+  top += '"id": "';
+  top += formJSON.id;
+  top += '",';
+  top += '\n';
+  top += content;
+  top += '"';
+  top += '\n';
+  top += '}';
+  top;
+  //console.log(top);
 
 
-}
+  const btn = document.createElement("button");
+  btn.setAttribute('type', 'button');
+  btn.innerHTML = "Submit Project";
+  inner2.appendChild(btn).onclick = function() {
+    addResults();
+  }
 
-var x = 1;
+  const btn2 = document.createElement("button");
+  btn2.setAttribute('type', 'button');
+  btn2.innerHTML = "Edit Project";
+  inner2.appendChild(btn2).onclick = function() {
+    addResults2();
+  }
 
-function addFields(){
+  function addResults() {
+    console.log(project_json);
+    //var xhr = new XMLHttpRequest();
+    //xhr.open("PUT", "https://b1ljlllud4.execute-api.us-east-1.amazonaws.com/items" , true);
+    //xhr.send(top);
 
-    // Get the element where the inputs will be added to
-    var container = document.getElementById("container");
-    // Remove every children it had before
-    while (container.hasChildNodes()) {
-        container.removeChild(container.lastChild);
-    }
-    for (i=0;i<window.x;i++){
 
-        container.appendChild(document.createElement("hr"));
-        container.appendChild(document.createElement("br"));
+  }
 
-        const para = document.createElement("p");
-        const textNode = document.createTextNode("Major Milestone " + (i+2) + ": ");
-        para.appendChild(textNode);
+  function addResults2() {
+    console.log(top);
+    //var xhr = new XMLHttpRequest();
+    //xhr.open("PUT", "https://b1ljlllud4.execute-api.us-east-1.amazonaws.com/items" , true);
+    //xhr.send(top);
+  }
 
-        // Append a node with a random text
-        container.appendChild(para);
-        // Create an <input> element, set its type and name attributes
-        var input = document.createElement("input");
-        input.type = "text";
-        input.name = ("milestone" + (i + 2));
-        input.placeholder = "Name ";
-        container.appendChild(input);
+  document.getElementById("submit").style.visibility = 'hidden';
 
-        var input2 = document.createElement("input");
-        input2.type = "date";
-        input2.name = ("baseline_due_date" + (i + 2));
-        const para2 = document.createElement("p");
-        var textNode2 = document.createTextNode("Baseline Due Date: ");
-        para2.appendChild(textNode2);
-        container.appendChild(para2);
-        container.appendChild(input2);
+  const para = document.createElement("h2");
+  const textNode = document.createTextNode("Review Project");
+  para.appendChild(textNode);
+  inner2.appendChild(para);
+  inner2.appendChild(document.createElement("hr"));
 
-        var input3 = document.createElement("input");
-        input3.type = "date";
-        input3.name = ("revised_due_date" + (i + 2));
-        const para3 = document.createElement("p");
-        var textNode3 = document.createTextNode("Revised Due Date: ");
-        para3.appendChild(textNode3);
-        container.appendChild(para3);
-        container.appendChild(input3);
+  const para2 = document.createElement("p");
+  const textNode2 = document.createTextNode("");
+  para2.appendChild(textNode2);
+  inner3.appendChild(para2);
+  inner3.appendChild(document.createElement("hr"));
 
-        container.appendChild(document.createElement("br"));
-        const para4 = document.createElement("p");
-        var textNode4 = document.createTextNode("Status: ");
-        para4.appendChild(textNode4);
-        container.appendChild(para4);
-
-        //Create array of options to be added
-        var array = ["Future","In-Process","At Risk","Delayed","Completed"];
-
-        //Create and append select list
-        var selectList = document.createElement("select");
-        selectList.name = ("status" + (i + 2));
-        container.appendChild(selectList);
-
-        //Create and append the options
-        for (var a = 0; a < array.length; a++) {
-            var option = document.createElement("option");
-            option.setAttribute("value", array[a]);
-            option.text = array[a];
-            selectList.appendChild(option);
-        }
-
-        var input5 = document.createElement("input");
-        input5.type = "number";
-        input5.min = 1;
-        input5.max = 100;
-        input5.name = ("percent_complete" + (i + 2));
-        input5.placeholder = "% Complete ";
-        container.appendChild(input5);
-
-        var textarea = document.createElement("textarea");
-        textarea.type = "text";
-        textarea.name = ("comments" + (i + 2));
-        textarea.rows = "6";
-        textarea.cols = "55";
-        textarea.placeholder = "Comments ";
-        container.appendChild(textarea);
-    }
-
-    window.x++
-
-    const btn = document.createElement("button");
-    btn.innerHTML = "Delete";
-    container.appendChild(btn).onclick = function() {removeFields();}
-
-    function removeFields( ){
-            window.x--
-
-            var times = 15;
-
-            for(var i = 0; i < times; i++){
-              container.removeChild(container.lastChild);
-            }
-
-            if (window.x >= 2){
-              container.appendChild(btn).onclick = function() {removeFields();}
-            }
-    }
-}
-
-var y = 1;
-
-function addFields2(){
-
-    // Get the element where the inputs will be added to
-    var container2 = document.getElementById("container2");
-    // Remove every children it had before
-    while (container2.hasChildNodes()) {
-        container2.removeChild(container2.lastChild);
-    }
-    for (i=0;i<window.y;i++){
-
-        container2.appendChild(document.createElement("hr"));
-        container2.appendChild(document.createElement("br"));
-
-        const para = document.createElement("p");
-        const textNode = document.createTextNode("Issue/Risk " + (i+2) + ": ");
-        para.appendChild(textNode);
-
-        // Append a node with a random text
-        container2.appendChild(para);
-        // Create an <input> element, set its type and name attributes
-        var input = document.createElement("input");
-        input.type = "text";
-        input.name = ("issue-risk" + (i + 2));
-        input.placeholder = "Name ";
-        container2.appendChild(input);
-
-        var input2 = document.createElement("input");
-        input2.type = "date";
-        input2.name = ("due_date" + (i + 2));
-        const para2 = document.createElement("p");
-        var textNode2 = document.createTextNode("Due Date: ");
-        para2.appendChild(textNode2);
-        container2.appendChild(para2);
-        container2.appendChild(input2);
-
-        var input3 = document.createElement("input");
-        input3.type = "date";
-        input3.name = ("revised_due_date" + (i + 2));
-        const para3 = document.createElement("p");
-        var textNode3 = document.createTextNode("Revised Due Date: ");
-        para3.appendChild(textNode3);
-        container2.appendChild(para3);
-        container2.appendChild(input3);
-
-        container2.appendChild(document.createElement("br"));
-        const para4 = document.createElement("p");
-        var textNode4 = document.createTextNode("Type: ");
-        para4.appendChild(textNode4);
-        container2.appendChild(para4);
-
-        //Create array of options to be added
-        var array = ["Issue","Risk","Decision"];
-
-        //Create and append select list
-        var selectList = document.createElement("select");
-        selectList.name = ("type" + (i + 2));
-        container2.appendChild(selectList);
-
-        //Create and append the options
-        for (var a = 0; a < array.length; a++) {
-            var option = document.createElement("option");
-            option.setAttribute("value", array[a]);
-            option.text = array[a];
-            selectList.appendChild(option);
-        }
-
-        var textarea = document.createElement("textarea");
-        textarea.type = "text";
-        textarea.name = ("mitigation" + (i + 2));
-        textarea.rows = "6";
-        textarea.cols = "55";
-        textarea.placeholder = "Mitigation ";
-        container2.appendChild(textarea);
-    }
-
-    window.y++
-
-    const btn = document.createElement("button");
-    btn.innerHTML = "Delete";
-    container2.appendChild(btn).onclick = function() {removeFields2();}
-
-    function removeFields2( ){
-            window.y--
-
-            var times = 13;
-
-            for(var i = 0; i < times; i++){
-              container2.removeChild(container2.lastChild);
-            }
-
-            if (window.y >= 2){
-              container2.appendChild(btn).onclick = function() {removeFields2();}
-            }
-    }
 }
 
 
 const form = document.querySelector('.contact-form');
 form.addEventListener('submit', handleFormSubmit);
+
+var x = 1;
+
+function addFields() {
+
+  // Get the element where the inputs will be added to
+  var container = document.getElementById("container");
+  // Remove every children it had before
+  while (container.hasChildNodes()) {
+    container.removeChild(container.lastChild);
+  }
+  for (i = 0; i < window.x; i++) {
+
+    container.appendChild(document.createElement("hr"));
+    container.appendChild(document.createElement("br"));
+
+    const para = document.createElement("p");
+    const textNode = document.createTextNode("Major Milestone " + (i + 2) + ": ");
+    para.appendChild(textNode);
+
+    // Append a node with a random text
+    container.appendChild(para);
+    // Create an <input> element, set its type and name attributes
+    var input = document.createElement("input");
+    input.type = "text";
+    input.name = ("milestone" + (i + 2));
+    input.placeholder = "Name ";
+    container.appendChild(input);
+
+    var input2 = document.createElement("input");
+    input2.type = "date";
+    input2.name = ("baseline_due_date" + (i + 2));
+    const para2 = document.createElement("p");
+    var textNode2 = document.createTextNode("Baseline Due Date: ");
+    para2.appendChild(textNode2);
+    container.appendChild(para2);
+    container.appendChild(input2);
+
+    var input3 = document.createElement("input");
+    input3.type = "date";
+    input3.name = ("revised_due_date" + (i + 2));
+    const para3 = document.createElement("p");
+    var textNode3 = document.createTextNode("Revised Due Date: ");
+    para3.appendChild(textNode3);
+    container.appendChild(para3);
+    container.appendChild(input3);
+
+    container.appendChild(document.createElement("br"));
+    const para4 = document.createElement("p");
+    var textNode4 = document.createTextNode("Status: ");
+    para4.appendChild(textNode4);
+    container.appendChild(para4);
+
+    //Create array of options to be added
+    var array = ["Future", "In-Process", "At Risk", "Delayed", "Completed"];
+
+    //Create and append select list
+    var selectList = document.createElement("select");
+    selectList.name = ("status" + (i + 2));
+    container.appendChild(selectList);
+
+    //Create and append the options
+    for (var a = 0; a < array.length; a++) {
+      var option = document.createElement("option");
+      option.setAttribute("value", array[a]);
+      option.text = array[a];
+      selectList.appendChild(option);
+    }
+
+    var input5 = document.createElement("input");
+    input5.type = "number";
+    input5.min = 1;
+    input5.max = 100;
+    input5.name = ("percent_complete" + (i + 2));
+    input5.placeholder = "% Complete ";
+    container.appendChild(input5);
+
+    var textarea = document.createElement("textarea");
+    textarea.type = "text";
+    textarea.name = ("comments" + (i + 2));
+    textarea.rows = "6";
+    textarea.cols = "55";
+    textarea.placeholder = "Comments ";
+    container.appendChild(textarea);
+  }
+
+  window.x++
+
+  const btn = document.createElement("button");
+  btn.setAttribute('type', 'button');
+  btn.innerHTML = "Delete";
+  container.appendChild(btn).onclick = function() {
+    removeFields();
+  }
+
+  function removeFields() {
+    window.x--
+
+    var times = 14;
+
+    for (var i = 0; i < times; i++) {
+      container.removeChild(container.lastChild);
+    }
+
+    if (window.x >= 2) {
+      container.appendChild(btn).onclick = function() {
+        removeFields();
+      }
+    }
+  }
+}
+
+var y = 1;
+
+function addFields2() {
+
+  // Get the element where the inputs will be added to
+  var container2 = document.getElementById("container2");
+  // Remove every children it had before
+  while (container2.hasChildNodes()) {
+    container2.removeChild(container2.lastChild);
+  }
+  for (i = 0; i < window.y; i++) {
+
+    container2.appendChild(document.createElement("hr"));
+    container2.appendChild(document.createElement("br"));
+
+    const para = document.createElement("p");
+    const textNode = document.createTextNode("Issue/Risk " + (i + 2) + ": ");
+    para.appendChild(textNode);
+
+    // Append a node with a random text
+    container2.appendChild(para);
+    // Create an <input> element, set its type and name attributes
+    var input = document.createElement("input");
+    input.type = "text";
+    input.name = ("issue-risk" + (i + 2));
+    input.placeholder = "Name ";
+    container2.appendChild(input);
+
+    var input2 = document.createElement("input");
+    input2.type = "date";
+    input2.name = ("due_date" + (i + 2));
+    const para2 = document.createElement("p");
+    var textNode2 = document.createTextNode("Due Date: ");
+    para2.appendChild(textNode2);
+    container2.appendChild(para2);
+    container2.appendChild(input2);
+
+    var input3 = document.createElement("input");
+    input3.type = "date";
+    input3.name = ("revised_due_date" + (i + 2));
+    const para3 = document.createElement("p");
+    var textNode3 = document.createTextNode("Revised Due Date: ");
+    para3.appendChild(textNode3);
+    container2.appendChild(para3);
+    container2.appendChild(input3);
+
+    container2.appendChild(document.createElement("br"));
+    const para4 = document.createElement("p");
+    var textNode4 = document.createTextNode("Type: ");
+    para4.appendChild(textNode4);
+    container2.appendChild(para4);
+
+    //Create array of options to be added
+    var array = ["Issue", "Risk", "Decision"];
+
+    //Create and append select list
+    var selectList = document.createElement("select");
+    selectList.name = ("type" + (i + 2));
+    container2.appendChild(selectList);
+
+    //Create and append the options
+    for (var a = 0; a < array.length; a++) {
+      var option = document.createElement("option");
+      option.setAttribute("value", array[a]);
+      option.text = array[a];
+      selectList.appendChild(option);
+    }
+
+    var textarea = document.createElement("textarea");
+    textarea.type = "text";
+    textarea.name = ("mitigation" + (i + 2));
+    textarea.rows = "6";
+    textarea.cols = "55";
+    textarea.placeholder = "Mitigation ";
+    container2.appendChild(textarea);
+  }
+
+  window.y++
+
+  const btn = document.createElement("button");
+  btn.setAttribute('type', 'button');
+  btn.innerHTML = "Delete";
+  container2.appendChild(btn).onclick = function() {
+    removeFields2();
+  }
+
+  function removeFields2() {
+    window.y--
+
+    var times = 13;
+
+    for (var i = 0; i < times; i++) {
+      container2.removeChild(container2.lastChild);
+    }
+
+    if (window.y >= 2) {
+      container2.appendChild(btn).onclick = function() {
+        removeFields2();
+      }
+    }
+  }
+}
